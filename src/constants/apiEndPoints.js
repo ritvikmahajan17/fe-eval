e71ec58ed6fbe5c1de89de0a49f94d06c5a3cc34
Ritvik Mahajan <ritvikmahajan2001@gmail.com>
2023-02-28 22:12:20 +0530

    initial commit

---
+++ src/constants/apiEndPoints.js
@@ -0,0 +1,17 @@
+export const BACKEND_URL = 'http://localhost:8080/api/';
+//TODO:Change Backend URL
+
+// export const GET_SONG_DATA = {
+//   url:'records',
+//   method:'get'
+// };
+
+// export const GET_SONG_LIKE_BY_ID = (id) => ({
+//   url:`records/${id}/likes`,
+//   method:'get'
+// });
+
+// export const UPDATE_SONG_LIKE_BY_ID = (id) => ({
+//   url:`records/${id}/likes`,
+//   method:'patch'
+// });
